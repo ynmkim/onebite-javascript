19ffc3f1a4e25d24747c4c1a8f8c971a2c6bee6d
ynmkim <younmeet@gmail.com>
2024-09-10 11:17:44 +0900

    feat: Day01 완료

---
+++ Day01/src/index.js
@@ -0,0 +1,19 @@
+// 미션1. 두 변수 선언 후 더한 값 출력하기
+// let 키워드를 사용해서 변수 num1, num2를 선언한 다음, 각각 숫자 10과 25를 할당해주세요.
+// 그리고 console.log를 사용해서 num1 변수와 num2 변수를 더한 값을 출력해주세요. (두 변수를 더할 때에는 + 기호를 사용해주세요)
+
+let num1 = 10;
+let num2 = 25;
+
+console.log(num1 + num2);
+
+// 미션2. 자료형 출력하기
+// 숫자형, 문자형, undefined 형 변수를 각각 선언한 다음, typeof 연산자를 사용해서 변수들의 자료형을 출력해주세요.
+
+let num = 123;
+let str = 'ABC';
+let und;
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof und);
