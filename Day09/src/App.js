4e781b2b280aee1de63dd8c502e638e37dd418d2
ynmkim <younmeet@gmail.com>
2024-10-03 15:59:12 +0900

    feat: Day09 완료

---
+++ Day09/src/App.js
@@ -0,0 +1,10 @@
+// 웹 페이지에 필요한 상태들을 App.js의 this.state 내부에 작성 후 제출하세요.
+
+export default function App($app) {
+  this.state = {
+    pokemonType: '',
+    pokemonList: [],
+    searchWord: '',
+    startIdx: 0,
+  };
+}
---
+++ Day09/src/components/api.js
@@ -0,0 +1,1 @@
+const API_URL = 'https://pokemon-api-ecru-eta.vercel.app/';
