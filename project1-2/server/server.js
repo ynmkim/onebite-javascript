79c32c38f5aa9ec9f1a2b05fcee3535b58a50df6
ynmkim <younmeet@gmail.com>
2024-10-03 18:29:48 +0900

    refactor: 프로젝트 동물 앨범 만들기 3
    새로고침 시 404 에러 발생 문제 해결

---
+++ project1-2/server/server.js
@@ -0,0 +1,23 @@
+// 1. npm init -y
+// 2. npm install express
+
+const express = require('express');
+//node.js의 프레임워크인 express 모듈 불러오기
+const path = require('path');
+// 파일 시스템의 경로를 다루기 위해 사용되는 path 모듈 불러오기
+const app = express();
+// express 함수를 호출해서 express 애플리케이션 객체를 생성
+const PORT = 3000;
+//서버가 계속해서 듣고 있을 포트 번호 설정
+
+app.use(express.static(path.join(__dirname, '..')));
+// express 애플리케이션에 미들웨어 추가
+// 정적 파일을 제공하는 미들웨어 함수
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log('START SERVER');
+});
