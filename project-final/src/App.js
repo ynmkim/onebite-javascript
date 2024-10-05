a33ba60f18d1fd53ecd1a8fdfc7a77859bf42941
ynmkim <younmeet@gmail.com>
2024-10-06 00:28:40 +0900

    feat: Header 컴포넌트 구현

--- project-final/src/App.js
+++ project-final/src/App.js
@@ -13,7 +13,58 @@ export default function App($app) {
     cities: '',
   };
 
-  const header = new Header();
+  const header = new Header({
+    $app,
+    initialState: {
+      sortBy: this.state.sortBy,
+      searchWord: this.state.searchWord,
+    },
+    handleSortChange: async (sortBy) => {
+      const pageUrl = `/${this.state.region}?sort=${sortBy}`;
+      history.pushState(
+        null,
+        null,
+        this.state.searchWord
+          ? pageUrl + `&search=${this.state.searchWord}`
+          : pageUrl
+      );
+      const cities = await getCities(
+        0,
+        this.state.region,
+        sortBy,
+        this.state.searchWord
+      );
+
+      this.setState({
+        ...this.state,
+        startIdx: 0,
+        sortBy: sortBy,
+        cities: cities,
+      });
+    },
+
+    handleSearchWord: async (searchWord) => {
+      history.pushState(
+        null,
+        null,
+        `/${this.state.region}?sort=${this.state.sortBy}?/&search=${searchWord}`
+      );
+
+      const cities = await getCities(
+        0,
+        this.state.region,
+        this.state.sortBy,
+        searchWord
+      );
+
+      this.setState({
+        ...this.state,
+        startIdx: 0,
+        searchWord: searchWord,
+        cities: cities,
+      });
+    },
+  });
   const regionList = new RegionList();
   const cityList = new CityList({
     $app,
@@ -41,6 +92,10 @@ export default function App($app) {
   this.setState = (newState) => {
     this.state = newState;
     cityList.setState(this.state.cities);
+    header.setState({
+      sortBy: this.state.sortBy,
+      searchWord: this.state.searchWord,
+    });
   };
 
   const init = async () => {
