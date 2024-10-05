b997c44d645b83840479102d72cde218bd8515fa
ynmkim <younmeet@gmail.com>
2024-10-05 21:14:50 +0900

    feat: Day12 pokemon 데이터 불러오기
    detail 페이지 이동, 포켓몬 type별 api 호출

--- Day12/src/App.js
+++ Day12/src/App.js
@@ -20,33 +20,39 @@ export default function App($app) {
   };
 
   const pokemonList = new PokemonList({
-    // 코드 작성
     $app,
     initialState: this.state.pokemonList,
-    //아이템을 클릭하면 "/detail/id" 로 이동할 수 있도록 아래의 함수를 완성하세요.
-    handleItemClick: async () => {},
-    //타입을 클릭하면, 클리한 타입에 해당하는 포켓몬만 띄워지고,
-    // "/type" 으로 이동할 수 있도록 아래의 함수를 완성하세요.
-    handleTypeClick: () => {},
+    handleItemClick: async (id) => {
+      history.pushState(null, null, `/detail/${id}`);
+      this.setState({
+        ...this.state,
+        currentPage: `/detail/${id}`,
+      });
+    },
+    handleTypeClick: async (type) => {
+      history.pushState(null, null, `/${type}`);
+      const pokemonList = await getPokemonList(type);
+      this.setState({
+        ...this.state,
+        pokemonList: pokemonList,
+        searchWord: getSearchWord(),
+        type: type,
+        currentPage: `/${type}`,
+      });
+    },
   });
 
   this.setState = (newState) => {
     this.state = newState;
-    // 코드 작성
-    // pokemonList의 state 값을 할당
     pokemonList.setState(this.state.pokemonList);
   };
 
   const init = async () => {
-    // 코드 작성 api 호출하여 초기 데이터 불러오기
     try {
-      let pokemonList = await getPokemonList(
-        this.state.type,
-        this.state.searchWord
-      );
+      const initialPokemonList = await getPokemonList();
       this.setState({
         ...this.state,
-        pokemonList: pokemonList,
+        pokemonList: initialPokemonList,
       });
     } catch (err) {
       console.log(err);
--- Day12/src/components/PokemonList.js
+++ Day12/src/components/PokemonList.js
@@ -20,18 +20,17 @@ export default function PokemonList({
   this.template = () => {
     // html 코드는 아래와 같이 제공드립니다.
     // 필요한 코드를 추가적으로 작성해 웹 사이트를 완성하세요.
-
     let temp = [];
     if (this.state) {
       this.state.forEach((elem, idx) => {
         temp += `<div class="pokemon-wrapper">
-          <div class="img-wrapper" id="${elem.name}">
-              <img src="${elem.img}" alt="${elem.name}"></img>
+          <div class="img-wrapper" id="${elem.id}">
+              <img src="${elem.img}" alt="${elem.name}" />
           </div>
           <div class="pokemon-info">
               <div class="index">No.${idx}</div>
               <div class="name">${elem.name}</div>
-              <div class="type">${elem.type}</div> 
+              <div class="type">${setPokemonType(elem.type)}</div> 
           </div>
         </div>`;
       });
@@ -41,6 +40,18 @@ export default function PokemonList({
 
   this.render = () => {
     this.$target.innerHTML = this.template();
+
+    this.$target.querySelectorAll('div.img-wrapper').forEach((elem) => {
+      elem.addEventListener('click', () => {
+        this.handleItemClick(elem.id);
+      });
+    });
+
+    this.$target.querySelectorAll('div.type-tag').forEach((elem) => {
+      elem.addEventListener('click', () => {
+        this.handleTypeClick(elem.id);
+      });
+    });
   };
 
   this.setState = (newState) => {
